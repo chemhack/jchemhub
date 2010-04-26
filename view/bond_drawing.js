e5aa3cbdaf7890379a20e5bffa20fd61a8ae5835
Paul Novak <novakps@gmail.com>
2010-04-26 11:24:57 -0400

    correct path to closure

--- view/bond_drawing.js
+++ view/bond_drawing.js
@@ -1,5 +1,6 @@
 goog.provide("jchemhub.view.BondDrawing");
 goog.require("jchemhub.view.Drawing");
+goog.require("jchemhub.math.Line");
 goog.require("goog.events");
 
 /**
@@ -13,6 +14,8 @@ goog.require("goog.events");
  */
 jchemhub.view.BondDrawing = function(x0, y0, x1, y1) {
 	jchemhub.view.Drawing.call(this);
+	this._line = new jchemhub.math.Line(new goog.math.Coordinate(x0, y0),
+			new goog.math.Coordinate(x1, y1));
 	this._coord0 = new goog.math.Coordinate(x0, y0);
 	this._coord1 = new goog.math.Coordinate(x1, y1);
 };
@@ -26,32 +29,37 @@ jchemhub.view.BondDrawing.prototype.getCoordinates = function() {
 };
 
 /**
- * render box to surround BondDrawing
+ * render box to surround BondDrawing with 
+ * width equal to highlight radius
  */
 jchemhub.view.BondDrawing.prototype.render = function() {
 	// render bond box path
 	var fill = new goog.graphics.SolidFill('red', 0.001);
 	var stroke = null;
-	var radius = this.getConfig().get("highlight").radius;
-	var slope = this.perpendicularSlope(new goog.math.Line(this._coord0.x,
-			this._coord0.y, this._coord1.x, this._coord1.y));
+	var radius = this.getConfig().get("highlight").radius * 3;
+	var theta = this._line.getTheta();
 
-	// shift centerline of bond by highlight radius to create box around bond
-	var trans_a = goog.graphics.AffineTransform.getTranslateInstance(slope.x
-			* radius, slope.y * radius);
-	var trans_b = goog.graphics.AffineTransform.getTranslateInstance(-slope.x
-			* radius, -slope.y * radius);
+	var angle_left = theta + (Math.PI / 2);
+	var angle_right = theta - (Math.PI / 2);
 
-	var coords_a = this.transformCoords(this.getTransform().clone().concatenate(trans_a), [
-			this._coord0, this._coord1 ]);
-	var coords_b = this.transformCoords(this.getTransform().clone()
-			.concatenate(trans_b), [ this._coord0, this._coord1 ]);
+	var transleft = goog.graphics.AffineTransform.getTranslateInstance(Math
+			.cos(angle_left)
+			* radius, Math.sin(angle_left) * radius, 0, 0, 0);
+
+	var transright = goog.graphics.AffineTransform.getTranslateInstance(Math
+			.cos(angle_right)
+			* radius, Math.sin(angle_right) * radius, 0, 0, 0);
+
+	var leftside = this.transformCoords(transleft, [this._line.getStart(), this._line.getEnd()]);
+	var rightside = this.transformCoords(transright, [this._line.getStart(), this._line.getEnd()]);
+
+	var boxCoords = this.transformCoords(this.getTransform(), [leftside[0], leftside[1], rightside[0], rightside[1]]);
 
 	bondBoxPath = new goog.graphics.Path();
-	bondBoxPath.moveTo(coords_a[0].x, coords_a[0].y);
-	bondBoxPath.lineTo(coords_a[1].x, coords_a[1].y);
-	bondBoxPath.lineTo(coords_b[1].x, coords_b[1].y);
-	bondBoxPath.lineTo(coords_b[0].x, coords_b[0].y);
+	bondBoxPath.moveTo(boxCoords[0].x, boxCoords[0].y);
+	bondBoxPath.lineTo(boxCoords[2].x, boxCoords[2].y);
+	bondBoxPath.lineTo(boxCoords[3].x, boxCoords[3].y);
+	bondBoxPath.lineTo(boxCoords[1].x, boxCoords[1].y);
 	bondBoxPath.close();
 
 	this.getGraphics().drawPath(bondBoxPath, stroke, fill, this.getGroup());
@@ -82,7 +90,7 @@ jchemhub.view.BondDrawing.prototype.highlightOn = function(e) {
 	if (slope.x == 0) {
 		angle = 0;
 	} else {
-		angle = ( -Math.atan(slope.x / slope.y) * 180 / Math.PI);
+		angle = (-Math.atan(slope.x / slope.y) * 180 / Math.PI);
 	}
 
 	var arcExtent;
@@ -94,11 +102,8 @@ jchemhub.view.BondDrawing.prototype.highlightOn = function(e) {
 	var coords = drawing.transformCoords(drawing.getTransform(), [
 			drawing._coord0, drawing._coord1 ]);
 	var path = new goog.graphics.Path();
-	path
-			.arc(coords[0].x, coords[0].y, radius, radius, angle,
-					arcExtent);
-	path.arc(coords[1].x, coords[1].y, radius, radius, angle,
-			-arcExtent);
+	path.arc(coords[0].x, coords[0].y, radius, radius, angle, arcExtent);
+	path.arc(coords[1].x, coords[1].y, radius, radius, angle, -arcExtent);
 
 	if (!drawing.bondHightlightGroup) {
 		drawing.bondHightlightGroup = drawing.getGraphics().createGroup();
--- view/single_bond_up_drawing.js
+++ view/single_bond_up_drawing.js
@@ -1,4 +1,5 @@
 goog.provide("jchemhub.view.SingleBondUpDrawing");
+goog.require("jchemhub.view.BondDrawing");
 
 /**
  * A single bond stereo up graphical element in the reaction editor.
@@ -20,18 +21,34 @@ goog.inherits(jchemhub.view.SingleBondUpDrawing, jchemhub.view.BondDrawing);
 jchemhub.view.SingleBondUpDrawing.prototype.render = function() {
 	// TTD implement wedge-shape
 	var bondPath = new goog.graphics.Path();
-	var bondStroke = new goog.graphics.Stroke(
-			this.getConfig().get("bond").stroke.width, this.getConfig().get(
-					"bond").stroke.color);
-	var bondFill = null;
-	
-	var coords = this.transformCoords(this.getTransform(), [this._coord0, this._coord1]);
+	var strokeWidth = this.getConfig().get("bond").stroke.width / 6;
+	var bondStroke = new goog.graphics.Stroke(strokeWidth, this.getConfig()
+			.get("bond").stroke.color);
+	var bondFill = new goog.graphics.SolidFill(
+			this.getConfig().get("bond").fill.color);
+
+	var theta = this._line.getTheta();
+	var angle_left = theta + (Math.PI / 2);
+	var angle_right = theta - (Math.PI / 2);
+
+	var trans1 = goog.graphics.AffineTransform.getTranslateInstance(Math
+			.cos(angle_left)
+			* strokeWidth, Math.sin(angle_left) * strokeWidth, 0, 0, 0);
+	var target1 = this.transformCoords(trans1, [ this._coord1 ])[0];
+	var trans2 = goog.graphics.AffineTransform.getTranslateInstance(Math
+			.cos(angle_right)
+			* strokeWidth, Math.sin(angle_right) * strokeWidth, 0, 0, 0);
+	var target2 = this.transformCoords(trans2, [ this._coord1 ])[0];
+
+	var coords = this.transformCoords(this.getTransform(), [ this._coord0,
+			target1, target2 ]);
 
 	bondPath.moveTo(coords[0].x, coords[0].y);
 	bondPath.lineTo(coords[1].x, coords[1].y);
+	bondPath.lineTo(coords[2].x, coords[2].y);
 
 	this.getGraphics()
 			.drawPath(bondPath, bondStroke, bondFill, this.getGroup());
-	
+
 	jchemhub.view.SingleBondUpDrawing.superClass_.render.call(this);
 }
