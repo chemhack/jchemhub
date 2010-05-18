1cb15d3440d2503ed5a4a4d70c48e3906b0d2a95
Paul Novak <novakps@gmail.com>
2010-05-18 10:57:34 -0400

    demo page

---
+++ json_test_rxn.js
@@ -0,0 +1,407 @@
+reactionDrawing = {
+	"reactants" : [ {
+		"atoms" : [ {
+			"coord" : {
+				"y" : 2.249999999999999,
+				"x" : 1.2990381056766584
+			},
+			"charge" : 0,
+			"symbol" : "C"
+		}, {
+			"coord" : {
+				"y" : 3.749999999999999,
+				"x" : 1.2990381056766616
+			},
+			"charge" : 0,
+			"symbol" : "O"
+		}, {
+			"coord" : {
+				"y" : 1.5,
+				"x" : 0
+			},
+			"charge" : 0,
+			"symbol" : "C"
+		}, {
+			"coord" : {
+				"y" : 0,
+				"x" : 0
+			},
+			"charge" : 0,
+			"symbol" : "C"
+		}, {
+			"coord" : {
+				"y" : 2.2500000000000004,
+				"x" : -1.2990381056766582
+			},
+			"charge" : 0,
+			"symbol" : "C"
+		}, {
+			"coord" : {
+				"y" : -0.7500000000000007,
+				"x" : 1.2990381056766576
+			},
+			"charge" : 0,
+			"symbol" : "O"
+		}, {
+			"coord" : {
+				"y" : -0.7500000000000001,
+				"x" : -1.2990381056766584
+			},
+			"charge" : 0,
+			"symbol" : "C"
+		}, {
+			"coord" : {
+				"y" : 1.5,
+				"x" : -2.5980762113533165
+			},
+			"charge" : 0,
+			"symbol" : "C"
+		}, {
+			"coord" : {
+				"y" : -2.220446049250313E-16,
+				"x" : -2.598076211353316
+			},
+			"charge" : 0,
+			"symbol" : "C"
+		} ],
+		"name" : null,
+		"bondindex" : [ {
+			"source" : 1,
+			"stereo" : "NOT_STEREO",
+			"target" : 0,
+			"type" : "SINGLE_BOND"
+		}, {
+			"source" : 2,
+			"stereo" : "NOT_STEREO",
+			"target" : 0,
+			"type" : "SINGLE_BOND"
+		}, {
+			"source" : 3,
+			"stereo" : "NOT_STEREO",
+			"target" : 2,
+			"type" : "DOUBLE_BOND"
+		}, {
+			"source" : 4,
+			"stereo" : "NOT_STEREO",
+			"target" : 2,
+			"type" : "SINGLE_BOND"
+		}, {
+			"source" : 5,
+			"stereo" : "NOT_STEREO",
+			"target" : 3,
+			"type" : "SINGLE_BOND"
+		}, {
+			"source" : 6,
+			"stereo" : "NOT_STEREO",
+			"target" : 3,
+			"type" : "SINGLE_BOND"
+		}, {
+			"source" : 4,
+			"stereo" : "NOT_STEREO",
+			"target" : 7,
+			"type" : "DOUBLE_BOND"
+		}, {
+			"source" : 8,
+			"stereo" : "NOT_STEREO",
+			"target" : 6,
+			"type" : "DOUBLE_BOND"
+		}, {
+			"source" : 7,
+			"stereo" : "NOT_STEREO",
+			"target" : 8,
+			"type" : "SINGLE_BOND"
+		} ]
+	}, {
+		"atoms" : [ {
+			"coord" : {
+				"y" : 0,
+				"x" : 9.294228634059946
+			},
+			"charge" : 0,
+			"symbol" : "C"
+		}, {
+			"coord" : {
+				"y" : 1.5,
+				"x" : 9.294228634059946
+			},
+			"charge" : 0,
+			"symbol" : "C"
+		}, {
+			"coord" : {
+				"y" : 2.2500000000000018,
+				"x" : 7.995190528383289
+			},
+			"charge" : 0,
+			"symbol" : "O"
+		}, {
+			"coord" : {
+				"y" : 2.249999999999999,
+				"x" : 10.593266739736604
+			},
+			"charge" : 0,
+			"symbol" : "O"
+		}, {
+			"coord" : {
+				"y" : 3.7500000000000018,
+				"x" : 7.995190528383293
+			},
+			"charge" : 0,
+			"symbol" : "C"
+		}, {
+			"coord" : {
+				"y" : 4.500000000000006,
+				"x" : 6.696152422706637
+			},
+			"charge" : 0,
+			"symbol" : "C"
+		}, {
+			"coord" : {
+				"y" : 4.499999999999998,
+				"x" : 9.294228634059952
+			},
+			"charge" : 0,
+			"symbol" : "O"
+		} ],
+		"name" : null,
+		"bondindex" : [ {
+			"source" : 1,
+			"stereo" : "NOT_STEREO",
+			"target" : 0,
+			"type" : "SINGLE_BOND"
+		}, {
+			"source" : 2,
+			"stereo" : "NOT_STEREO",
+			"target" : 1,
+			"type" : "SINGLE_BOND"
+		}, {
+			"source" : 3,
+			"stereo" : "NOT_STEREO",
+			"target" : 1,
+			"type" : "DOUBLE_BOND"
+		}, {
+			"source" : 4,
+			"stereo" : "NOT_STEREO",
+			"target" : 2,
+			"type" : "SINGLE_BOND"
+		}, {
+			"source" : 5,
+			"stereo" : "NOT_STEREO",
+			"target" : 4,
+			"type" : "SINGLE_BOND"
+		}, {
+			"source" : 6,
+			"stereo" : "NOT_STEREO",
+			"target" : 4,
+			"type" : "DOUBLE_BOND"
+		} ]
+	} ],
+	"products" : [ {
+		"atoms" : [ {
+			"coord" : {
+				"y" : 3.749999999999999,
+				"x" : 21.186533479473216
+			},
+			"charge" : 0,
+			"symbol" : "O"
+		}, {
+			"coord" : {
+				"y" : 2.249999999999999,
+				"x" : 21.186533479473212
+			},
+			"charge" : 0,
+			"symbol" : "C"
+		}, {
+			"coord" : {
+				"y" : 1.4999999999999973,
+				"x" : 22.48557158514987
+			},
+			"charge" : 0,
+			"symbol" : "O"
+		}, {
+			"coord" : {
+				"y" : 1.5,
+				"x" : 19.887495373796554
+			},
+			"charge" : 0,
+			"symbol" : "C"
+		}, {
+			"coord" : {
+				"y" : 0,
+				"x" : 19.887495373796554
+			},
+			"charge" : 0,
+			"symbol" : "C"
+		}, {
+			"coord" : {
+				"y" : 2.2500000000000004,
+				"x" : 18.588457268119896
+			},
+			"charge" : 0,
+			"symbol" : "C"
+		}, {
+			"coord" : {
+				"y" : -0.7500000000000007,
+				"x" : 21.186533479473212
+			},
+			"charge" : 0,
+			"symbol" : "O"
+		}, {
+			"coord" : {
+				"y" : -0.7500000000000001,
+				"x" : 18.588457268119896
+			},
+			"charge" : 0,
+			"symbol" : "C"
+		}, {
+			"coord" : {
+				"y" : 1.5,
+				"x" : 17.289419162443238
+			},
+			"charge" : 0,
+			"symbol" : "C"
+		}, {
+			"coord" : {
+				"y" : -2.250000000000001,
+				"x" : 21.186533479473212
+			},
+			"charge" : 0,
+			"symbol" : "C"
+		}, {
+			"coord" : {
+				"y" : -2.220446049250313E-16,
+				"x" : 17.289419162443238
+			},
+			"charge" : 0,
+			"symbol" : "C"
+		}, {
+			"coord" : {
+				"y" : -3.0000000000000018,
+				"x" : 22.48557158514987
+			},
+			"charge" : 0,
+			"symbol" : "C"
+		}, {
+			"coord" : {
+				"y" : -3.0000000000000004,
+				"x" : 19.887495373796554
+			},
+			"charge" : 0,
+			"symbol" : "O"
+		} ],
+		"name" : null,
+		"bondindex" : [ {
+			"source" : 1,
+			"stereo" : "NOT_STEREO",
+			"target" : 0,
+			"type" : "DOUBLE_BOND"
+		}, {
+			"source" : 2,
+			"stereo" : "NOT_STEREO",
+			"target" : 1,
+			"type" : "SINGLE_BOND"
+		}, {
+			"source" : 3,
+			"stereo" : "NOT_STEREO",
+			"target" : 1,
+			"type" : "SINGLE_BOND"
+		}, {
+			"source" : 4,
+			"stereo" : "NOT_STEREO",
+			"target" : 3,
+			"type" : "DOUBLE_BOND"
+		}, {
+			"source" : 5,
+			"stereo" : "NOT_STEREO",
+			"target" : 3,
+			"type" : "SINGLE_BOND"
+		}, {
+			"source" : 6,
+			"stereo" : "NOT_STEREO",
+			"target" : 4,
+			"type" : "SINGLE_BOND"
+		}, {
+			"source" : 7,
+			"stereo" : "NOT_STEREO",
+			"target" : 4,
+			"type" : "SINGLE_BOND"
+		}, {
+			"source" : 5,
+			"stereo" : "NOT_STEREO",
+			"target" : 8,
+			"type" : "DOUBLE_BOND"
+		}, {
+			"source" : 9,
+			"stereo" : "NOT_STEREO",
+			"target" : 6,
+			"type" : "SINGLE_BOND"
+		}, {
+			"source" : 10,
+			"stereo" : "NOT_STEREO",
+			"target" : 7,
+			"type" : "DOUBLE_BOND"
+		}, {
+			"source" : 8,
+			"stereo" : "NOT_STEREO",
+			"target" : 10,
+			"type" : "SINGLE_BOND"
+		}, {
+			"source" : 11,
+			"stereo" : "NOT_STEREO",
+			"target" : 9,
+			"type" : "SINGLE_BOND"
+		}, {
+			"source" : 12,
+			"stereo" : "NOT_STEREO",
+			"target" : 9,
+			"type" : "DOUBLE_BOND"
+		} ]
+	}, {
+		"atoms" : [ {
+			"coord" : {
+				"y" : 0,
+				"x" : 29.181724007856502
+			},
+			"charge" : 0,
+			"symbol" : "C"
+		}, {
+			"coord" : {
+				"y" : 1.5,
+				"x" : 29.181724007856502
+			},
+			"charge" : 0,
+			"symbol" : "C"
+		}, {
+			"coord" : {
+				"y" : 2.2500000000000018,
+				"x" : 27.882685902179844
+			},
+			"charge" : 0,
+			"symbol" : "O"
+		}, {
+			"coord" : {
+				"y" : 2.249999999999999,
+				"x" : 30.48076211353316
+			},
+			"charge" : 0,
+			"symbol" : "O"
+		} ],
+		"name" : null,
+		"bondindex" : [ {
+			"source" : 1,
+			"stereo" : "NOT_STEREO",
+			"target" : 0,
+			"type" : "SINGLE_BOND"
+		}, {
+			"source" : 2,
+			"stereo" : "NOT_STEREO",
+			"target" : 1,
+			"type" : "SINGLE_BOND"
+		}, {
+			"source" : 3,
+			"stereo" : "NOT_STEREO",
+			"target" : 1,
+			"type" : "DOUBLE_BOND"
+		} ]
+	} ],
+	"header" : null
+}
