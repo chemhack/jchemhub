5f2a2eaf3d3b068d6eccb60cd76d40c856664d20
Paul Novak <novakps@gmail.com>
2010-05-31 15:47:10 -0400

    add dependencies to build

---
+++ export.js
@@ -0,0 +1,4 @@
+
+goog.exportSymbol("goog.events.listen", goog.events.listen);
+goog.exportSymbol("goog.events.EventType.LOAD", goog.events.EventType);
+goog.exportSymbol("goog.debug.Console", goog.debug.Console);
--- jchemhub/controller/reaction_editor.js
+++ jchemhub/controller/reaction_editor.js
@@ -11,11 +11,6 @@ goog.require('goog.editor.BrowserFeature');
 goog.require('goog.async.Delay');
 goog.require('jchemhub.controller.Plugin');
 
-// these are needed for the demo.html
-//goog.exportSymbol("goog.events.listen", goog.events.listen);
-//goog.exportSymbol("goog.events.EventType.LOAD", goog.events.EventType);
-//goog.exportSymbol("goog.debug.Console", goog.debug.Console);
-
 
 /**
  * A graphical editor for reactions
